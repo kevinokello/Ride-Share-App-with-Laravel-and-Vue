84cdcdac2a727bf111bc5ad2e9ff36039c66b433
kevinokello <okellokevin77@gmail.com>
2023-04-16 20:10:09 +0300

    trip process

--- backend/app/Http/Controllers/TripController.php
+++ backend/app/Http/Controllers/TripController.php
@@ -34,4 +34,51 @@ class TripController extends Controller
         }
         return response()->json(['message' => 'Cannot find this trip'], 404);
     }
+    public function accept(Request $request, Trip $trip)
+    {
+        $request->validate([
+            'driver_location' => 'required'
+
+        ]);
+        $trip->update([
+            'driver_id' => $request->user()->id,
+            'driver_location' => $request->driver_location,
+
+        ]);
+        // $trip->load('driver');
+        // $trip->driver->load('user');
+        $trip->load('driver.user');
+        return $trip;
+    }
+    public function start(Request $request, Trip $trip)
+    {
+        $trip->update([
+            'is_started' => true
+
+        ]);
+        $trip->load('driver.user');
+        return $trip;
+    }
+    public function end(Request $request, Trip $trip)
+    {
+        $trip->update([
+            'is_complete' => true
+
+        ]);
+        $trip->load('driver.user');
+        return $trip;
+    }
+    public function location(Request $request, Trip $trip)
+    {
+        $request->validate([
+            'driver_location' => 'required'
+
+        ]);
+        //update driver's current location
+        $trip->update([
+            'driver_location' => $request->driver_location
+        ]);
+        $trip->load('driver.user');
+        return $trip;
+    }
 }
